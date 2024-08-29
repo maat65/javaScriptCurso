63fb0ab07030b2c5481effe861722f2d34b0898e
Mateus Yoon <mateusyoon0605@hotmail.com>
2024-08-29 13:09:05 -0300

    atualizando

---
+++ main.js
@@ -0,0 +1,10 @@
+const nome = "Pedro"
+const sobrenome = "Araujo"
+
+const nomes = "Augusto, Paulo, Felipe"
+
+// console.log("O nome dele é " + nome + " " + sobrenome) || Esse modo não é mais usado 
+// console.log(`O nome dele é ${nome.toUpperCase()} ${sobrenome.toLowerCase()} `) // Modo mais utilizado nos dias de hoje
+
+// console.log(nomes.split(","))
+
